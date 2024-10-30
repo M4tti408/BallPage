941a6c73efba991d401911be6236111f74621e62
M4tti408 <mattia.harlacher@noseryoung.ch>
2024-10-30 12:03:20 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  distDir: 'build',
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+};
+
+module.exports = nextConfig;
